59e3dae869e56691e0b67df7faf906fcd1308866
sushma <sushmap9544@gmail.com>
2026-02-03 15:57:34 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api-aptitude.vindiainfrasec.com/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
